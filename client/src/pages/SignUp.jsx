d90c959c308b4687dd5fa6f41935e3b83400e65d
Akhilakku23 <akhilsaji0031@gmail.com>
2025-03-03 15:56:30 +0530

    signup page submit fuction

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,23 +1,72 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value, 
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData); 
+
+   
+    setFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
 
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username' />
-        <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' />
-        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          className="border p-3 rounded-lg"
+          id="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          id="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          id="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          type="submit"
+        >
           Sign up
         </button>
       </form>
 
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link to="/sign-in" className='text-blue-700 hover:underline'>
+        <Link to="/sign-in" className="text-blue-700 hover:underline">
           Sign in
         </Link>
       </div>
